e3180e9bcaf01d2df466b4cc3ef848476fb91d1d
luisflpereira <luis@valispace.com>
2025-08-12 13:59:52 +0100

    feat: add articles list and article detail pages

---
+++ app/pages/articles/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Article {{ articleId }}</h1>
+    <p>Article details go here...</p>
+    <NuxtLink to="/articles">Back to list</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const articleId = route.params.id
+</script>
---
+++ app/pages/articles/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Articles List</h1>
+    <ul>
+      <li v-for="article in articles" :key="article.id">
+        <NuxtLink :to="`/articles/${article.id}`">{{ article.title }}</NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const articles = [
+  { id: 1, title: 'Article 1' },
+  { id: 2, title: 'Article 2' },
+  { id: 3, title: 'Article 3' },
+]
+</script>
